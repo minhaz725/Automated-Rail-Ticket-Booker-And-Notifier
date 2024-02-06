8ba9c4a02539c4078d95c8a863a584e8a97af7f6
minhaz <minhaz725@gmail.com>
2024-02-07 02:38:20 +0600

    added coach selection by generic, made some successful test. Code is complete for work

--- main/search.go
+++ main/search.go
@@ -93,48 +93,56 @@ func performSearch(url string) (string, bool) {
 			var example string
 			err := chromedp.Run(ctx,
 				chromedp.Evaluate(`(() => {
-        const headers = Array.from(document.querySelectorAll('h2'));
-        const header = headers.find(h => h.innerText === '`+constants.SPECIFIC_TRAIN+`');
-        if (!header) throw new Error('Header not found');
-        const appSingleTrip = header.closest('app-single-trip');
-        if (!appSingleTrip) throw new Error('Parent component not found');
+					const headers = Array.from(document.querySelectorAll('h2'));
+        			const header = headers.find(h => h.innerText === '`+constants.SPECIFIC_TRAIN+`');
+        			if (!header) throw new Error('Header not found');
+        			const appSingleTrip = header.closest('app-single-trip');
+        			if (!appSingleTrip) throw new Error('Parent component not found');
         
-        // Filter single-seat-class divs by the text content of the seat-class-name span
-        const seatClassDivs = Array.from(appSingleTrip.querySelectorAll('.single-seat-class'));
-        const seatDiv = seatClassDivs.find(div => {
-            const seatNameSpan = div.querySelector('.seat-class-name');
-            return seatNameSpan && seatNameSpan.innerText.trim() === '`+constants.SEAT_TYPE+`';
-        });
-        if (!seatDiv) throw new Error('Seat class div not found');
+        			// Filter single-seat-class divs by the text content of the seat-class-name span
+        			const seatClassDivs = Array.from(appSingleTrip.querySelectorAll('.single-seat-class'));
+        			const seatDiv = seatClassDivs.find(div => {
+            		const seatNameSpan = div.querySelector('.seat-class-name');
+            		return seatNameSpan && seatNameSpan.innerText.trim() === '`+constants.SEAT_TYPE+`';
+        			});
+        			if (!seatDiv) throw new Error('Seat class div not found');
         
-        // Find and click the book now button within the specific seat class div
-        const bookNowBtn = seatDiv.querySelector('.book-now-btn-wrapper .book-now-btn');
-        if (!bookNowBtn) throw new Error('Book now button not found');
-        bookNowBtn.click();
+        			// Find and click the book now button within the specific seat class div
+        			const bookNowBtn = seatDiv.querySelector('.book-now-btn-wrapper .book-now-btn');
+        			if (!bookNowBtn) throw new Error('Book now button not found');
+        			bookNowBtn.click();
 		
-		setTimeout(() => {
-			 const bogieSelection = document.getElementById('select-bogie');
-			if (!bogieSelection) throw new Error('Bogie selection dropdown not found');
-			bogieSelection.value = `+constants.CoachMap[constants.COACH_NUMB]+`;
-			// Dispatch an input event to simulate user interaction
-			bogieSelection.dispatchEvent(new Event('change', { bubbles: true }));
-       			setTimeout(() => {
-			
-            	const seatOne = document.querySelector('.btn-seat.seat-available[title="`+constants.COACH_NUMB+constants.SEAT_ONE_NUMB+`"]');
-            	if (!seatOne) throw new Error('seatOne button not found');
-            	seatOne.click();
-				const seatTwo = document.querySelector('.btn-seat.seat-available[title="`+constants.COACH_NUMB+constants.SEAT_TWO_NUMB+`"]');
-            	if (!seatTwo) throw new Error('seatTwo button not found');
-            	seatTwo.click();
-					//setTimeout(() => {
-					//				const continueButton = document.querySelector('.continue-btn');
-					//				if (!continueButton) throw new Error('Continue Purchase button not found');
-					//				continueButton.click();
-		 			//			},  500); 
-        		},  500); // Delay of  1000 milliseconds (1 second)
-			},  500); 
+					setTimeout(() => {
+			 		// Find the select element
+        				const bogieSelection = document.getElementById('select-bogie');
+        				if (!bogieSelection) throw new Error('Bogie selection dropdown not found');
+        
+ 						// Find the option that contains the coach numb
+        				const coachOption = Array.from(bogieSelection.options).find(option => option.text.includes('`+constants.COACH_NUMB+`'));
+        				if (!coachOption) throw new Error('Option with text `+constants.COACH_NUMB+` not found');
+        
+        				// Set the selected option to the one found
+        				bogieSelection.value = coachOption.value;
+        				// Dispatch an input event to simulate user interaction
+        				bogieSelection.dispatchEvent(new Event('change', { bubbles: true }));
 
-    })()`, &example),
+       					setTimeout(() => {
+			
+            				const seatOne = document.querySelector('.btn-seat.seat-available[title="`+constants.COACH_NUMB+constants.SEAT_ONE_NUMB+`"]');
+            				if (!seatOne) throw new Error('seatOne button not found');
+            				seatOne.click();
+							const seatTwo = document.querySelector('.btn-seat.seat-available[title="`+constants.COACH_NUMB+constants.SEAT_TWO_NUMB+`"]');
+            				if (!seatTwo) throw new Error('seatTwo button not found');
+            				seatTwo.click();
+								setTimeout(() => {
+									const continueButton = document.querySelector('.continue-btn');
+									if (!continueButton) throw new Error('Continue Purchase button not found');
+									continueButton.click();
+		 						},  500); 
+        					},  500); // Delay of  1000 milliseconds (1 second)
+						},  500); 
+
+    			})()`, &example),
 				chromedp.Sleep(2*time.Second),
 			)
 			if err != nil {
--- utils/constants/constants.go
+++ utils/constants/constants.go
@@ -4,9 +4,9 @@ const (
 	BASE_URL               = "https://eticket.railway.gov.bd/booking/train/search/?"
 	FROM                   = "fromcity=Dhaka"
 	TO                     = "&tocity=Cox%27s%20Bazar"
-	DATE                   = "&doj=10-Feb-2024"
+	DATE                   = "&doj=09-Feb-2024"
 	CLASS                  = "&class=S_CHAIR"
-	SEAT_COUNT             = 2
+	SEAT_COUNT             = 1
 	SENDER_EMAIL_ADDRESS   = "minhaz725@gmail.com"
 	SENDER_EMAIL_PASSWORD  = "akey whwp pnul eskw"
 	RECEIVER_EMAIL_ADDRESS = "atiabintiaziz@gmail.com"
@@ -14,18 +14,6 @@ const (
 	SPECIFIC_TRAIN         = "PARJOTAK EXPRESS (816)"
 	SEAT_TYPE              = "SNIGDHA"
 	COACH_NUMB             = "SCHA"
-	SEAT_ONE_NUMB          = "-51"
+	SEAT_ONE_NUMB          = "-8"
 	SEAT_TWO_NUMB          = "-12"
 )
-
-var CoachMap = map[string]string{
-	"KA":   "0",
-	"TA":   "1",
-	"THA":  "2",
-	"DA":   "3",
-	"SCHA": "4",
-	"DANT": "5",
-	"TO":   "6",
-	"THO":  "7",
-	"DOA":  "8",
-}
