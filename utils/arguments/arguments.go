a1e767faf5820168f0344230da909075ec9840d1
minhaz <minhaz725@gmail.com>
2024-03-23 12:32:24 +0600

    cmd argument taking done commit fix

---
+++ utils/arguments/arguments.go
@@ -0,0 +1,39 @@
+package arguments
+
+import (
+	"Rail-Ticket-Notifier/utils/constants"
+	"flag"
+	"strings"
+)
+
+var (
+	FROM                 string
+	TO                   string
+	DATE                 string
+	SEAT_COUNT           uint
+	SEAT_TYPE_ARRAY      []string
+	SPECIFIC_TRAIN_ARRAY []string
+)
+
+func init() {
+	flag.StringVar(&FROM, "from", "Chattogram", "From city")
+	flag.StringVar(&TO, "to", "Dhaka", "To city")
+	flag.StringVar(&DATE, "date", "25-Mar-2024", "Date of travel")
+	flag.UintVar(&SEAT_COUNT, "seatCount", 2, "Seat count")
+
+	flag.Func("seatTypes", "Seat types", func(s string) error {
+		SEAT_TYPE_ARRAY = strings.Split(s, ",")
+		return nil
+	})
+
+	flag.Func("trains", "Specific trains", func(s string) error {
+		SPECIFIC_TRAIN_ARRAY = strings.Split(s, ",")
+		return nil
+	})
+
+	flag.Parse()
+}
+
+func GetURL() string {
+	return constants.BASE_URL + "fromcity=" + FROM + "&tocity=" + TO + "&doj=" + DATE + "&class=" + SEAT_TYPE_ARRAY[0]
+}
--- utils/constants/constants.go
+++ utils/constants/constants.go
@@ -3,18 +3,8 @@ package constants
 const (
 	BASE_URL               = "https://eticket.railway.gov.bd/booking/train/search/?"
 	DEBUG_CHROME_URL       = "http://localhost:9222"
-	FROM                   = "fromcity=Chattogram"
-	TO                     = "&tocity=Dhaka"
-	DATE                   = "&doj=26-Feb-2024"
-	CLASS                  = "&class=S_CHAIR"
-	SEAT_COUNT             = 2
 	SENDER_EMAIL_ADDRESS   = "minhaz725@gmail.com"
 	SENDER_EMAIL_PASSWORD  = "akey whwp pnul eskw"
 	RECEIVER_EMAIL_ADDRESS = "atiabintiaziz@gmail.com"
 	SEARCH_DELAY_IN_SEC    = 2
-)
-
-var (
-	SEAT_TYPE_ARRAY      = []string{"SNIGDHA", "S_CHAIR"}
-	SPECIFIC_TRAIN_ARRAY = []string{"TURNA", "PARJOTAK", "BAZAR"}
 )
