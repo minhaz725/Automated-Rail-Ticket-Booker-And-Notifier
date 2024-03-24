781263a04f86a8b7c666e208bd69e8d94c1e00b5
minhaz <minhaz725@gmail.com>
2024-03-25 03:57:46 +0600

    main file bug ?

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"Rail-Ticket-Notifier/internal/ui"
+	"fyne.io/fyne/v2/container"
+)
+
+// sample arg: go run . -from "Khulna" -to "Dhaka" -date "26-Mar-2024" -seatCount 4 -seatTypes "SNIGDHA,S_CHAIR" -trains "TURNA,SUNDARBAN,BAZAR"
+// fyne package -os windows -icon C:\Users\minha\go\src\Rail-Ticket-Notifier\static\logo.png -src cmd/main
+
+func main() {
+
+	elementsOfUI := ui.InitializeUIAndForm()
+
+	form := ui.CreateForm(elementsOfUI)
+
+	elementsOfUI.Window.SetContent(container.NewVBox(elementsOfUI.IntroLabel, form))
+	elementsOfUI.Window.ShowAndRun()
+}
