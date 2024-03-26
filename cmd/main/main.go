b8e648732408fb92c765b13e960d4bc6ff6f19a2
minhaz <minhaz725@gmail.com>
2024-03-26 15:55:37 +0600

    search logic fixed, class updadted

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,7 +7,7 @@ import (
 
 // sample arg: go run . -from "Khulna" -to "Dhaka" -date "26-Mar-2024" -seatCount 4 -seatTypes "SNIGDHA,S_CHAIR" -trains "TURNA,SUNDARBAN,BAZAR"
 // fyne package -os windows -icon C:\Users\minha\go\src\Rail-Ticket-Notifier\static\logo.png -src cmd/main
-
+// start chrome.exe --remote-debugging-port=9222
 func main() {
 
 	elementsOfUI := ui.InitializeUIAndForm()
--- internal/arguments/arguments.go
+++ internal/arguments/arguments.go
@@ -13,22 +13,22 @@ var (
 	DATE                   string
 	SEAT_COUNT             uint
 	PHONE_NUMBER           string
-	SEAT_TYPE_ARRAY        = []string{"SNIGDHA", "S_CHAIR", "F_BERTH"}
-	SPECIFIC_TRAIN_ARRAY   = []string{"SUBORNO"} //{"SONAR", "TURNA", "SUBORNO"}
+	SEAT_TYPE_ARRAY        = []string{"F_BERTH", "AC_B"} //, "S_CHAIR", "F_BERTH"}
+	SPECIFIC_TRAIN_ARRAY   = []string{"TURNA"}           //{"SONAR", "TURNA", "SUBORNO"}
 	RECEIVER_EMAIL_ADDRESS string
 	GO_TO_BOOK_PAGE        uint
 )
 
 func init() {
 	now := time.Now()
-	twoDaysLater := now.AddDate(0, 0, 3)
+	twoDaysLater := now.AddDate(0, 0, 9)
 	formattedDateAfterTwoDays := twoDaysLater.Format("02-Jan-2006")
 
-	flag.StringVar(&FROM, "from", "Chattogram", "From city")
-	flag.StringVar(&TO, "to", "Dhaka", "To city")
+	flag.StringVar(&FROM, "from", "Dhaka", "From city")
+	flag.StringVar(&TO, "to", "Chattogram", "To city")
 	flag.StringVar(&PHONE_NUMBER, "phone", "+8801555555555", "Phone")
 	flag.StringVar(&DATE, "date", formattedDateAfterTwoDays, "Date of travel")
-	flag.StringVar(&RECEIVER_EMAIL_ADDRESS, "email", "minhaz725@gmail.com", "Email address")
+	flag.StringVar(&RECEIVER_EMAIL_ADDRESS, "email", "atiabintiaziz@gmail.com", "Email address")
 	flag.UintVar(&SEAT_COUNT, "seatCount", 2, "Seat count")
 	flag.UintVar(&GO_TO_BOOK_PAGE, "purchasePage", 1, "Go to purchase page")
 
